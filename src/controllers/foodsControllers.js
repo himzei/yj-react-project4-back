c863c14939322c509550dc3f9473df82e67436b9
조현일 <himzei@gmail.com>
2023-07-29 15:57:37 +0900

    re

--- src/controllers/foodsControllers.js
+++ src/controllers/foodsControllers.js
@@ -1,13 +1,9 @@
+import axios from "axios";
+
 export const getJoonguFoods = async (req, res) => {
   // api 요철
-  fetch(
-    "https://www.daegufood.go.kr/kor/api/tasty.html?mode=json&addr=%EC%A4%91%EA%B5%AC",
-    {
-      method: "GET",
-    }
-  )
-    .then((res) => res.json())
-    .then((data) => {
-      res.json({ ok: "true", data });
-    });
+  axios({
+    method: "GET",
+    url: "https://www.daegufood.go.kr/kor/api/tasty.html?mode=json&addr=%EC%A4%91%EA%B5%AC",
+  }).then((response) => res.send(response.data));
 };
