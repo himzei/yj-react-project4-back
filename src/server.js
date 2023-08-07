45d27e864dca9ffa1e5eab08628da83318f874a8
himzei <himzei@gmail.com>
2023-08-07 13:21:36 +0900

    cors option

--- src/server.js
+++ src/server.js
@@ -10,7 +10,10 @@ const PORT = 8080;
 const app = express();
 
 const corsOptions = {
-  origin: ["http://localhost:3000"],
+  origin: [
+    "http://localhost:3000",
+    "https://merry-starship-9d7e2f.netlify.app",
+  ],
   methods: ["GET", "POST"],
   credentials: true,
 };
