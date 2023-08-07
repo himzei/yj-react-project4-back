6076d131063bf13a5cb766ae70bb73857334020d
himzei <himzei@gmail.com>
2023-08-07 13:26:35 +0900

    re

--- build/server.js
+++ build/server.js
@@ -11,7 +11,7 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "d
 var PORT = 8080;
 var app = (0, _express["default"])();
 var corsOptions = {
-  origin: ["http://localhost:3000"],
+  origin: ["http://localhost:3000", "https://merry-starship-9d7e2f.netlify.app"],
   methods: ["GET", "POST"],
   credentials: true
 };
